371fbd3d13f25d1e83e700cff63dc1b0f36776e7
Anna Favis <anfavis@gmail.com>
2019-10-15 19:51:58 -0700

    Added jupyter notebook to add data files to sql and csv files in resources folder

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
+-- Drop existing tables (if any)
+DROP TABLE SF_movie_locations;
+DROP TABLE film_genre_awards;
+DROP TABLE film_runtime_awards;
+DROP TABLE mojo_top_grossing;
+
+
 -- Create Table for SF Movie Locations
 CREATE TABLE SF_movie_locations (
   id INT PRIMARY KEY,
@@ -7,11 +14,36 @@ CREATE TABLE SF_movie_locations (
 );
 
 --Create Table for FIlm Genres
-CREATE TABLE film_genre_awards (
-    Movie_Name VARCHAR(50),
-    Genre VARCHAR(50),
-    Run_Time INT,
-    Director VARCHAR(50),
-    Awards VARCHAR(50)
+CREATE TABLE film_runtime_awards (
+	id SERIAL PRIMARY KEY,
+  title VARCHAR,
+  genre VARCHAR,
+  runtime INT,
+  director VARCHAR,
+  awards VARCHAR
 );
 
+-- Create table for box office mojo
+CREATE TABLE mojo_top_grossing (
+mojo_id SERIAL PRIMARY KEY,
+rank INT,
+title VARCHAR,
+studio VARCHAR(20),
+worldwide VARCHAR,
+domestic VARCHAR,
+perc_domestic VARCHAR,
+overseas VARCHAR, 
+perc_overseas VARCHAR,
+year VARCHAR (10)
+);
+
+-- Create Table for movies from kaggle
+CREATE TABLE kaggle_movies (
+id SERIAL PRIMARY KEY,
+title VARCHAR,
+budget VARCHAR,
+revenue VARCHAR,
+release_date DATE TIME,
+imdb_id VARCHAR,
+original_language VARCHAR
+);
