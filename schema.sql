0f8e4471fbabb90967ce29a4a48aa296fcb7f9c7
beckykwarren <beckykwarren@gmail.com>
2019-10-15 18:33:29 -0700

    schema files updates

--- schema.sql
+++ schema.sql
@@ -1,8 +1,17 @@
--- Create Tables
+-- Create Table for SF Movie Locations
 CREATE TABLE SF_movie_locations (
   id INT PRIMARY KEY,
   title TEXT,
   release_year INT,
   locations TEXT
+);
+
+--Create Table for FIlm Genres
+CREATE TABLE film_genre_awards (
+    Movie_Name VARCHAR(50),
+    Genre VARCHAR(50),
+    Run_Time INT,
+    Director VARCHAR(50),
+    Awards VARCHAR(50)
 );
 
